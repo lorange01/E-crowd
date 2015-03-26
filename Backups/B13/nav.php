346b1a1a9575d49e1f4a60dffc4165b1c2d283c8
lorange01 <beguite15@gmail.com>
2015-03-26 04:09:08 +0100

    end_first view

---
+++ Backups/B13/nav.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../css/nav.css" rel="stylesheet">
+	
+		
+
+		<!-- Load JavaScript Libraries -->
+		<script src="../js/jquery.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+	</head>
+		
+	
+	<div class="container">
+						
+				<nav class="col-ms-2"  style="background-color:#4AA141;">          
+						<ul class="nav nav-pills nav-stacked">
+						  <li class="active"><a href="#">Home</a></li>
+						  <li><a href="#">Profile</a></li>
+						  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Dropdown <span class="caret"></span></a>							
+								<ul class="dropdown-menu">
+								  <li><a href="#">Action</a></li>
+								  <li><a href="#">Another action</a></li>
+								  <li><a href="#">Something else here</a></li>
+								  <li class="divider"></li>
+								  <li><a href="#">Separated link</a></li>
+								</ul>
+							</li>
+						</ul>
+				</nav>
+				
+				
+				<div class="col-ms-8">
+					  <div class="panel panel-default">
+							<div class="panel-heading">
+							  <h3 class="panel-title">Bienvenue sur mon site !</h3>
+							</div>	
+					  </div>
+				</div>	
+				<div class="col-ms-2" style="background-color:#4141BB;">
+					
+				</div>	
+							
+	</div>	
+	
+</body>
--- php/header3.php
+++ php/header3.php
@@ -19,135 +19,149 @@
 	</head>
 	
 	<body>
-<div class="container">
-<header class="navbar navbar-bright" role="banner">
+	<div class="container on first2">
+		<header class="navbar navbar-bright" role="banner">
+				<div class="row">
+				<div class="col-xs-4">
+				<a href="/" ><img id="size_logo" src="../images/images_png/logo-menu.png" /> </a>
+				<ul class="nav navbar-nav on connexion"><li><a href="#">Connexion</a></li></ul>
 
+				</div>
+			
+					<div class="col-xs-8">
+					
+					<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					 </button>
+			<nav class="collapse navbar-collapse" role="navigation">
+					  <ul class="nav navbar-right navbar-nav">
+							<li class="dropdown">
+							  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-search"></i></a>
+							<ul class="dropdown-menu" style="padding:12px;">
+								<form class="form-inline">
+								  <button type="submit" class="btn btn-default pull-right"><i class="glyphicon glyphicon-search"></i></button><input type="text" class="form-control pull-left" placeholder="Rechercher">
+								</form>
+							</ul>
+							</li>
+					  </ul>
+					  <ul class="nav navbar-nav ">
+						
+						<li><a href="#">Projet</a></li>				
+						<li><a href="#">Evènements</a></li>				
+						<li><a href="#">Communauté</a></li> 
+					  </ul>
+					  </nav>
+					</div>
+				</div>
+				<div class="row on space"><div class="col-xs-12"></div></div>
 
-
-		<div class="row">
-		<div class="col-xs-4">
-		<a href="/" ><img id="size_logo" src="../images/ecrowd_2.png" /> </a>
-
-		</div>
-			<div class="col-xs-8">
-			<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			 </button>
-	<nav class="collapse navbar-collapse" role="navigation">
-			  <ul class="nav navbar-right navbar-nav">
-					<li class="dropdown">
-					  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-search"></i></a>
-					<ul class="dropdown-menu" style="padding:12px;">
-						<form class="form-inline">
-						  <button type="submit" class="btn btn-default pull-right"><i class="glyphicon glyphicon-search"></i></button><input type="text" class="form-control pull-left" placeholder="Rechercher">
-						</form>
-					</ul>
-					</li>
-			  </ul>
-			  <ul class="nav navbar-nav ">
-				<li><a href="#">Projet</a></li>				
-				<li><a href="#">Evènements</a></li>				
-				<li><a href="#">Communauté</a></li> 
-			  </ul>
-			  </nav>
-			</div>
-		</div>
-		<div class="row on space"><div class="col-xs-12"></div></div>
-
-		
-		<div class="row on circle_logo">
-		<div class="col-xs-5"></div> 
-		<div class="col-xs-4 on circle_log ">
-		<img id="circle_log" src="../images/circ_ecrowd.png"/>
-		</div>  
-		</div>
-		<div class="row on circle_logo">
-		<div class="col-xs-5"></div> 
-		<div class="col-xs-4">
-		<h3>E-CROWD LE RÉSEAU DES FREELANCES BOOSTER VOTRE IDÉE</h3>
-		</div>  
-		</div>
-		<div class="row on join_us">
-		<div class="col-xs-5"></div> 
-		<div class="col-xs-4">
-		<button type="button" class="btn btn-danger"><a href="#">REJOIGNEZ-NOUS</a></button>
-		</div>  
-		</div>
-</header>
-
-		<div class ="mao">
-		<div id="cbp-so-scroller" class="cbp-so-scroller">
+				
+				<div class="row on circle_logo">
+				
+				<div class="col-xs-4.centered ">
+				<img id="circle_log" src="../images/images_png/logo-home.png"/>
+				<h3>E-CROWD LE RÉSEAU DES FREELANCES BOOSTER VOTRE IDÉE</h3>
+				<button type="button" class="btn btn-danger"><a href="#">REJOIGNEZ-NOUS</a></button>
+				</div>  
+				</div>
+				
+		</header>
+	</div>
+	<div class ="container on kemar">
+			<div id="cbp-so-scroller" class="cbp-so-scroller">
+				
+				<section class="cbp-so-section">
+					<article class="cbp-so-side cbp-so-side-left">
+						<h2>Teaser Vidéo</h2>
+						<p>Soufflé bonbon jelly cotton candy liquorice dessert jelly bear claw candy canes. Pudding halvah bonbon marzipan powder. Marzipan gingerbread sweet jelly.</p>
+					</article>
+					<figure class="cbp-so-side cbp-so-side-right">
+						<img src="../images/images_png/Marc_events.png" alt="teaser"/>
+					</figure>
+				</section>
+				
 				<section class="cbp-so-section">
 					<article class="cbp-so-side cbp-so-side-left">
 						<h2>Projets</h2>
 						<p>Fruitcake toffee jujubes. Topping biscuit sesame snaps jelly caramels jujubes tiramisu fruitcake. Marzipan tart lemon drops chocolate sesame snaps jelly beans.</p>
 					</article>
 					<figure class="cbp-so-side cbp-so-side-right">
-						<img src="../images/1.png" alt="img01">
+						<img src="../images/images_png/Marc_projet.png" alt="projet"/>
 					</figure>
 				</section>
+				
 				<section class="cbp-so-section">
 					<figure class="cbp-so-side cbp-so-side-left">
-						<img src="../images/2.png" alt="img01">
+						<img src="../images/images_png/Marc_events.png" alt="events"/>
 					</figure>
-					<article class="cbp-so-side cbp-so-side-right">
-						<h2>Évènements</h2>
-						<p>Lollipop powder danish sugar plum caramels liquorice sweet cookie. Gummi bears caramels gummi bears candy canes cheesecake sweet roll icing dragée. Gummies jelly-o tart. Cheesecake unerdwear.com candy canes apple pie halvah chocolate tiramisu.</p>
-					</article>
+						<article class="cbp-so-side cbp-so-side-right">
+							<h2>Évènements</h2>
+							<p>Lollipop powder danish sugar plum caramels liquorice sweet cookie. Gummi bears caramels gummi bears candy canes cheesecake sweet roll icing dragée. Gummies jelly-o tart. Cheesecake unerdwear.com candy canes apple pie halvah chocolate tiramisu.</p>
+						</article>
 				</section>
+				
 				<section class="cbp-so-section">
 					<article class="cbp-so-side cbp-so-side-left">
 						<h2>Communauté</h2>
 						<p>Soufflé bonbon jelly cotton candy liquorice dessert jelly bear claw candy canes. Pudding halvah bonbon marzipan powder. Marzipan gingerbread sweet jelly.</p>
 					</article>
 					<figure class="cbp-so-side cbp-so-side-right">
-						<img src="../images/3.png" alt="img01">
+						<img src="../images/images_png/Marc_communaute.png" alt="communaute"/>
 					</figure>
 				</section>
-				<section class="cbp-so-section">
-					<article class="cbp-so-side cbp-so-side-left">
-						<h2>Profils</h2>
-						<p>Fruitcake toffee jujubes. Topping biscuit sesame snaps jelly caramels jujubes tiramisu fruitcake. Marzipan tart lemon drops chocolate sesame snaps jelly beans.</p>
-					</article>
-					<figure class="cbp-so-side cbp-so-side-right">
-						<img src="../images/1.png" alt="img01">
-					</figure>
-				</section>
+
 				<section class="cbp-so-section">
 					<figure class="cbp-so-side cbp-so-side-left">
-						<img src="../images/2.png" alt="img01">
+						<img src="../images/images_png/Marc_events.png"" alt="QSN"/>
 					</figure>
 					<article class="cbp-so-side cbp-so-side-right">
 						<h2>Qui Sommes-Nous</h2>
 						<p>Lollipop powder danish sugar plum caramels liquorice sweet cookie. Gummi bears caramels gummi bears candy canes cheesecake sweet roll icing dragée. Gummies jelly-o tart. Cheesecake unerdwear.com candy canes apple pie halvah chocolate tiramisu.</p>
 					</article>
 				</section>
-				<section class="cbp-so-section">
-					<article class="cbp-so-side cbp-so-side-left">
-						<h2>Crédit</h2>
-						<p>Soufflé bonbon jelly cotton candy liquorice dessert jelly bear claw candy canes. Pudding halvah bonbon marzipan powder. Marzipan gingerbread sweet jelly.</p>
-					</article>
-					<figure class="cbp-so-side cbp-so-side-right">
-						<img src="../images/3.png" alt="img01">
-					</figure>
-				</section>
+				
+				<script src="../js/classie.js"></script>
+				<script src="../js/cbpScroller.js"></script>
+				<script>
+					new cbpScroller( document.getElementById( 'cbp-so-scroller' ) );
+				</script>
+
+				
+			</div>
+	</div>
+		  
+	<div class="container on conteneur2">
+			
+					<div class="row">
+						<h3 id="conteneur2_h3">FREELANCES DU SECTEUR WEB</h3>
+					</div>
+					<div class="row">
+					<div class="col-xs-1"></div>
+					<div class="col-xs-10 ">
+						<div class="coloneList1"><a href="#"><img class="glyphi" src="../images/glyphicons/png/glyphicons-dev.png"/><br/>Developpement</a></div>	
+						<div class="coloneList1"><a href="#"><img class="glyphi" src="../images/glyphicons/png/glyphicons-brush.png"/><br/>Création &amp; Design</a></div>	
+						<div class="coloneList1"><a href="#"><img class="glyphi" src="../images/glyphicons/png/glyphicons-cogwheels.png"/><br/>Gestion de Projet</a></div>	
+						<div class="coloneList1"><a href="#"><img class="glyphi" src="../images/glyphicons/png/glyphicons-stats.png"/><br/>Communication &amp; Marketing</a></div>	
+						<div class="coloneList1"><a href="#"><img class="glyphi" src="../images/glyphicons/png/glyphicons-activity.png"/><br/>Production &amp; Gestion de contenu</a></div>	
+						<div class="coloneList1"><a href="#"><img class="glyphi" src="../images/glyphicons/png/glyphicons-router.png"/><br/>Réseaux</a></div>		
+					</div>
+					</div>
+	</div>
 	
-
- 		<script src="../js/classie.js"></script>
-		<script src="../js/cbpScroller.js"></script>
-		<script>
-			new cbpScroller( document.getElementById( 'cbp-so-scroller' ) );
-		</script>
-
-		
-	</div>
-	</div>
-		
-</div>
-
+	<footer class="container on footer_conteneur">
+				<div class="row">
+							<br/>
+					<div class="col-xs-4.centered">
+					<img id="circle_log" src="../images/images_png/logo-footer.png"/><br/>
+					<a href="mailto:contact@ecrowd.com" target="_top">contact@ecrowd.com</a>
+					</div>
+				</div>
+				<div class="row on end_footer">
+				</div>
+	</footer>
 	</body>
 	
 </html>
